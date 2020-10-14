19a28a8c7f4a1c26b9e2a301c3c8d999d83319d4
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-10-14 10:17:00 -0300

    Create article model and migration file with associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_204515) do
+ActiveRecord::Schema.define(version: 2020_10_14_131344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
