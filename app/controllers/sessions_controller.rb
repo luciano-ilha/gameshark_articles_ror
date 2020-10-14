8370c2ef2941ea2a301a020d0b26f07645ffc8fb
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-10-14 06:51:46 -0300

    Set root to index and create file, modify sessions and users controller and new user's view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      sessions[:user_id] = @user.id
+      redirect_to '/welcome'
+    else
+      redirect_to '/login'
+    end
   end
 
   def login
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'users#index'
+
   resources :users, only: [:new, :create]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
