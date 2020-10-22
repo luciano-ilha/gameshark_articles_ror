ef8827f3c2e60f725f6565cc62941814b239a5e2
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-10-22 15:08:21 -0300

    Modify article_spec file

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -12,5 +12,23 @@ RSpec.describe Article, type: :model do
       article.text = nil
       expect(article).to_not be_valid  
     end
+
+    it "should have a category" do
+      article.category_id = nil
+      expect(article).to_not be_valid  
+    end
+
+    it "should have an author" do
+      article.author = nil
+      expect(article).to_not be_valid  
+    end
+
+    it "should have a title, text, category and author" do
+      article.title = "Some Title"
+      article.text = "Some text"
+      article.category_id = "1"
+      article.user_id = "1"
+      expect(article).to be_valid  
+    end
   end
 end
